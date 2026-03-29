4d3ce95d45ced9b7cfa26d9d19c5a8b815d9c800
Prateek2757 <pratikguragain4@gmail.com>
2026-03-29 18:05:43 +0545

    update url of providers individual and make providers page color too

--- src/components/providers/provider-list.tsx
+++ src/components/providers/provider-list.tsx
@@ -57,7 +57,7 @@ export default function ProvidersGrid({ providers }: { providers: any[] }) {
       <div className="relative overflow-hidden border-b border-border">
         <div className="pointer-events-none absolute inset-0">
           <div className="absolute -top-32 left-1/4 h-96 w-96 rounded-full bg-amber-500/10 blur-[100px]" />
-          <div className="absolute -top-16 right-1/3 h-72 w-72 rounded-full bg-violet-500/10 blur-[80px]" />
+          <div className="absolute -top-16 right-1/3 h-72 w-72 rounded-full bg-blue-500/10 blur-[80px]" />
 
           <div
             className="absolute inset-0 opacity-30 pointer-events-none"
