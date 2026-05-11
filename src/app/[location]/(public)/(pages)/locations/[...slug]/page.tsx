c3b3bc4cd15dd4f9f0760f211d8175728031bf6b
Prateek2757 <pratikguragain4@gmail.com>
2026-05-11 15:06:30 +0545

    added static page in location for dynamic cacheing

--- src/app/[location]/(public)/(pages)/locations/[...slug]/page.tsx
+++ src/app/[location]/(public)/(pages)/locations/[...slug]/page.tsx
@@ -4,7 +4,9 @@ import Script from "next/script";
 import StatePageClient from "../components/state-page/state-page";
 import CityPageClient from "../components/City-page/citypageclient";
 import { getAllCities, getCategoriesFromDB } from "@/lib/cache";
+export const revalidate = 86400;
 
+export const dynamic = "force-static";
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
@@ -359,31 +361,31 @@ export default async function CityOrStatePage({ params }: Props) {
   );
 }
 
-// export async function generateStaticParams() {
-//   try {
-//     const res = await fetch(
-//       `${process.env.NEXT_PUBLIC_APP_URL}/api/service-location`
-//     );
-//     const cities: City[] = res.ok ? await res.json() : [];
+export async function generateStaticParams() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_APP_URL}/api/service-location`
+    );
+    const cities: City[] = res.ok ? await res.json() : [];
 
-//     const params: { slug: string[] }[] = [];
+    const params: { slug: string[] }[] = [];
 
-//     const statesSeen = new Set<string>();
-//     for (const c of cities) {
-//       if (c.state_slug && !statesSeen.has(c.state_slug)) {
-//         statesSeen.add(c.state_slug);
-//         params.push({ slug: [c.state_slug] });
-//       }
-//     }
+    const statesSeen = new Set<string>();
+    for (const c of cities) {
+      if (c.state_slug && !statesSeen.has(c.state_slug)) {
+        statesSeen.add(c.state_slug);
+        params.push({ slug: [c.state_slug] });
+      }
+    }
 
-//     for (const c of cities) {
-//       if (c.state_slug && c.slug) {
-//         params.push({ slug: [c.state_slug, c.slug] });
-//       }
-//     }
+    for (const c of cities) {
+      if (c.state_slug && c.slug) {
+        params.push({ slug: [c.state_slug, c.slug] });
+      }
+    }
 
-//     return params;
-//   } catch {
-//     return [];
-//   }
-// }
+    return params;
+  } catch {
+    return [];
+  }
+}
